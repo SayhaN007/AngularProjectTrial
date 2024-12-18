2e6fe3fd9f303f50a70cef58bb89445a353735c9
K S Sayhan <sayhansdhr1115@gmail.com>
2024-12-18 23:01:57 +0530

    Initial Web Application 18-12-24

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  imports: [],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardComponent {
+ 
+  @Input()data : any;
+
+}
